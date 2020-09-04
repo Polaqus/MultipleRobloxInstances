db11d15b75fee5c69a6520153738ade88bb6d9e1
Inversion <michael9050@hotmail.com>
2020-09-04 12:58:42 -0700

    Updated due to changes to the solution

--- MultipleRobloxInstances/Source.cpp
+++ MultipleRobloxInstances/Source.cpp
@@ -5,6 +5,7 @@ int main(int argc, char* argv[])
     HANDLE mutex = CreateMutexA(NULL, TRUE, "ROBLOX_singletonMutex");
 
     system("pause");
+
     bool ReleaseMutex(mutex);
     bool CloseHandle(mutex);
 }
